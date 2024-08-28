58814a921826627a9ed9290ed966b7a53f982eb0
hix <canbedir100@gmail.com>
2024-08-28 20:55:33 +0300

    feat: add Skeleton loading state to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,32 @@
+"use client";
 import React from "react";
-import SignInButton from "../Profile/SignInButton";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
+import SignInButton from "../Profile/SignInButton";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const { status } = useSession();
+
   return (
     <div className="w-full h-[80px] border-b shadow-md">
       <div className="flex items-center justify-between h-full text-2xl px-20">
         <div>
-          <Link href={"/"}>
-            <span className="font-">hix</span>
-            <span className="font-bold">Code</span>
-          </Link>
+          {status === "loading" ? (
+            <Skeleton className="w-28 h-10 rounded-md" />
+          ) : (
+            <Link href={"/"}>
+              <span className="font-">hix</span>
+              <span className="font-bold">Code</span>
+            </Link>
+          )}
         </div>
         <div>
-          <SignInButton />
+          {status === "loading" ? (
+            <Skeleton className="h-11 w-11 rounded-full" />
+          ) : (
+            <SignInButton />
+          )}
         </div>
       </div>
     </div>
